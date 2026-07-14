be07d13482773b7e90454f4df8b80f51e9e395f6
Lex043 <alexferanmi390@gmail.com>
2026-07-14 02:08:48 +0100

    add: terraform workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,15 @@ resource "aws_key_pair" "blog" {
   public_key = file("${path.module}/keys/blog-key.pub")
 }
 
+// terraform state bucket
+terraform {
+  backend "s3" {
+    bucket = "lex-blog-api-tfstate"
+    key    = "blog-api/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 // iam role for ssm
 resource "aws_iam_role" "ec2_ssm" {
   name = "ec2-ssm-role"
@@ -38,7 +47,7 @@ resource "aws_iam_role_policy_attachment" "ssm" {
 
 resource "aws_iam_instance_profile" "ec2" {
   name = "ec2-ssm-profile"
-  role = aws_iam_role.role.ec2_ssm.name
+  role = aws_iam_role.ec2_ssm.name
 }
 
 
